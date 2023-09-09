0962eec9fc9108bd6e227a8da5d93f29e0868259
Dusty Brigsby <dustybrigsby@gmail.com>
2023-09-09 13:02:20 -0700

    Created User model
    updated package.json with Prettier

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function (value) {
+                    // Regular expression for email validation
+                    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+                    return emailRegex.test(value);
+                },
+                message: 'Please enter a valid email address',
+            },
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user',
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema
+    .virtual('friendCount')
+    // Getter
+    .get(function () {
+        return this.friends.length;
+    });
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
