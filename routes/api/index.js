91a060e75b58e070ba39311139871328056b1cb6
Dusty Brigsby <dustybrigsby@gmail.com>
2023-09-10 07:46:40 -0700

    Create index.js for routes

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+router.use('/users', thoughtRoutes);
+router.use('/thoughts', userRoutes);
+
+module.exports = router;
