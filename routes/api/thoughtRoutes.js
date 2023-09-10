e7f76edb1f04e31faeb234fe5b9b8291bb2424c7
Dusty Brigsby <dustybrigsby@gmail.com>
2023-09-10 09:02:19 -0700

    Updated thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,9 +11,22 @@ const {
 } = require('../../controllers/thoughtController');
 
 // /api/thoughts
-
+router.route('/')
+    .get(getThoughts)
+    .post(createThought);
 
 // /api/thoughts/:thoughtId
-
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;
