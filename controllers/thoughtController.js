16ebeed16a401da7f19566994bca7524350fc86f
Dusty Brigsby <dustybrigsby@gmail.com>
2023-09-10 07:38:13 -0700

    Added outline code to controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,70 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+
+
+module.exports = {
+    // Get all thoughts
+    async getThoughts(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Get a single thought
+    async getSingleThought(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Create a new thought
+    async createThought(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Update a thought
+    async updateThought(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Delete a thought
+    async deleteThought(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Add a reaction to a thought
+    async addReaction(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Delete a reaction
+    async deleteReaction(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+};
---
+++ controllers/userController.js
@@ -0,0 +1,61 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+
+
+module.exports = {
+    // Get all users
+    async getUsers(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Get a single user
+    async getSingleUser(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Create a new user
+    async createUser(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Delete a  user
+    async deleteUser(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Add another user as a friend
+    async addFriend(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+    // Remove a friend
+    async removeFriend(req, res) {
+        try {
+
+        } catch (error) {
+
+        }
+    },
+
+};
